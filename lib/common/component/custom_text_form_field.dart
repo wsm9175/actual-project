c3ac0694caeabb5099c3a7fb653b4efffbd53b55
wsm9175 <wsm9175@naver.com>
2023-07-09 17:03:07 +0900

    setting login view

---
+++ lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../const/colors.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    required this.onChanged,
+    this.autofocus = false,
+    this.obscureText = false,
+    this.hintText,
+    this.errorText,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      )
+    );
+
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      // 비밀번호 입력할 때
+      obscureText: obscureText,
+      autofocus: autofocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20), // 입력받는 칸에 대한 패딩 설정
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+        // 모든 Input 상태의 기본 스타일 세팅
+        border: baseBorder,
+        enabledBorder: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          )
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/user/view/login_screen.dart
@@ -0,0 +1,93 @@
+import 'package:actual/common/const/colors.dart';
+import 'package:actual/common/layout/default_layout.dart';
+import 'package:flutter/material.dart';
+import '../../common/component/custom_text_form_field.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      child: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: SafeArea(
+          top: true,
+          bottom: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _Title(),
+                const SizedBox(height: 16.0,),
+                _SubTitle(),
+                Image.asset(
+                  'asset/img/misc/logo.png',
+                  width: MediaQuery.of(context).size.width / 3 * 2,
+                ),
+                CustomTextFormField(
+                  hintText: '이메일을 입력해주세요',
+                  onChanged: (String value) {},
+                ),
+                const SizedBox(height: 16.0,),
+                CustomTextFormField(
+                  hintText: '비밀번호를 입력해주세요',
+                  onChanged: (String value) {},
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16.0,),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: PRIMARY_COLOR,
+                  ),
+                  child: Text('로그인'),
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: Text('회원가입'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                    ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '환영합니다!',
+      style: TextStyle(
+        fontSize: 34,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+class _SubTitle extends StatelessWidget {
+  const _SubTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '이메일과 비밀번호를 입력해서 로그인 해주세요!\n오늘도 성공적인 주문이 되길 :)',
+      style: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: BODY_TEXT_COLOR,
+      ),
+    );
+  }
+}
